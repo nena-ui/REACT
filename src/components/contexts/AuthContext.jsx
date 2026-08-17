c8f603c527a3e0c70001ab9e896ffc250de48964
nena-ui <dangolnena12@gmail.com>
2026-08-17 21:02:03 +0545

    changes

--- src/components/contexts/AuthContext.jsx
+++ src/components/contexts/AuthContext.jsx
@@ -5,12 +5,14 @@ const AuthContext = createContext()
 export const AuthProvider = ({children})=> {
 
     const [token,setToken]  = useState(null)
+    // const [name, setName] = useState("");
     const [loading,setLoading] = useState(true)
 
         useEffect(()=> {
-            const tokenfromlocal = localStorage.getItem("token")
-
-            setToken(tokenfromlocal)
+            const token = localStorage.getItem("token")
+            // const name = localStorage.getItem("Aracs")
+            
+            setToken(token)
             setLoading(false)
 
         },[])
@@ -20,15 +22,10 @@ export const AuthProvider = ({children})=> {
     return(
 
 <AuthContext.Provider value={{token,setToken,loading}}>
+    {/* <AuthContext.Provider value={{ name, setName }}> */}
     {children}
 </AuthContext.Provider>
     )
 }
 export default AuthContext
-
-// const [name, setName] = useState("");
-
-//   return (
-//     <AuthContext.Provider value={{ name, setName }}>
-//       {children}
-//     </AuthContext.Provider>
+     
